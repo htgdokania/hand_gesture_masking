dd7ce2724baa8c7327eef89fd74cf2f0c920fdb6
harsh <dokaniaharsh@gmail.com>
2018-10-08 14:03:32 +0530

    blur_added

--- hand.py
+++ hand.py
@@ -6,18 +6,16 @@ cap=cv2.VideoCapture(0)
 while True:
     ret,frame=cap.read()
     hsv=cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
-    """type 1"""
-##    low_skin=np.array([0,20,70])
-##    up_skin=np.array([20,255,255])
-    """type 2"""
     low_skin=np.array([0,30,60])
     up_skin=np.array([20,150,255])
-
     mask=cv2.inRange(hsv,low_skin,up_skin)
     result=cv2.bitwise_and(frame,frame,mask=mask)
+    blur=cv2.GaussianBlur(mask,(15,15),0)
+
 ##    cv2.imshow('res',result)
 ##    cv2.imshow('frame',frame)
     cv2.imshow('mask',mask)
+    cv2.imshow('blur',blur)
 
     if cv2.waitKey(1) & 0xFF==ord('q'):
         break
