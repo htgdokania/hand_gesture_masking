475ef5b8961708ad8ec896a392b930a609dc2cca
harsh_htg <dokaniaharsh@gmail.com>
2018-10-08 06:18:08 +0000

    mask

--- hand.py
+++ hand.py
@@ -6,20 +6,18 @@ cap=cv2.VideoCapture(0)
 while True:
     ret,frame=cap.read()
     hsv=cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
-##    cv2.imshow('hsv',hsv)
     """type 1"""
-##    low_red=np.array([0,20,70])
-##    up_red=np.array([20,255,255])
+##    low_skin=np.array([0,20,70])
+##    up_skin=np.array([20,255,255])
     """type 2"""
-    low_red=np.array([0,30,60])
-    up_red=np.array([20,150,255])
+    low_skin=np.array([0,30,60])
+    up_skin=np.array([20,150,255])
 
-    mask=cv2.inRange(hsv,low_red,up_red)
+    mask=cv2.inRange(hsv,low_skin,up_skin)
     result=cv2.bitwise_and(frame,frame,mask=mask)
-
-    cv2.imshow('frame',frame)
+##    cv2.imshow('res',result)
+##    cv2.imshow('frame',frame)
     cv2.imshow('mask',mask)
-    cv2.imshow('res',result)
 
     if cv2.waitKey(1) & 0xFF==ord('q'):
         break
